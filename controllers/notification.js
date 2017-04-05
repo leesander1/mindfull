7dd40eb07896611ca509feb66b889983a1b1c6bc
Lee <lee@leesander.com>
2017-04-05 00:38:41 -0500

    update

--- controllers/notification.js
+++ controllers/notification.js
@@ -8,7 +8,7 @@ const User = require('../models/User');
 exports.pushMorning = () => {
   console.log('Morning push started...');
   let numbers = ['4698773526'];
-  User.find({ 'notification': {'morning':{$eq: true}}}, (err, user) => {
+  User.find({ 'notification': {'morning': true}}, (err, user) => {
   console.log(user);
     if (err) { return next(err); }
     //numbers.push(user.profile.phone);
