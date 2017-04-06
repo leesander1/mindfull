d1e088c5079558352c8735aab8ceba2ae5de7f66
Lee <lee@leesander.com>
2017-04-06 10:15:09 -0500

    heroku cron

--- cron_evening.js
+++ cron_evening.js
@@ -1,1 +1,5 @@
+const cronJob = require('cron').CronJob;
+const notificationController = require('./controllers/notification');
+
 console.log('Cron job successful');
+notificationController.pushEvening();
