1eb95ec4327149fb36affdd9f9386fc502dab9ac
Lee <lee@leesander.com>
2017-04-12 20:06:23 -0500

    push

--- cron.js
+++ cron.js
@@ -1,8 +1,5 @@
-const notificationController = require('./controllers/notification');
 
 function sayHello() {
-  notificationController.pushNow();
   console.log('Cron job successful');
 }
 sayHello();
-process.exit();
